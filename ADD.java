f8fca8aa177c10d43b2b8b53597ab22da24d8974
Annointed <mk@gmail.com>
2022-06-06 21:51:37 +0200

    first commit of the ADD.java project

---
+++ ADD.java
@@ -0,0 +1,12 @@
+public class ADD{
+	public static void main(String [] args){
+		
+		int x = 10;
+		int y = 10;
+		int z = x + y;
+
+		System.out.println("x= 10 + y = 10 is equal to " + z);
+
+
+	}
+}
